3a457cc66c902ca9f74d5d1cccd01f5900ea9d5f
Eddy Medvednic <17541522+medvednic@users.noreply.github.com>
2018-04-05 12:56:40 +0300

    keywords for api filter loaded from file

--- producer/producer.py
+++ producer/producer.py
@@ -14,17 +14,25 @@ logger.addHandler(fh)
 keep_fds = [fh.stream.fileno()]
 
 
+def read_file(file_name):
+    with open('config/' + file_name, encoding="utf-8") as f:
+        return json.load(f)
+
+
 def main():
     logging.info('Session start')
-    with open('tweeter-api-credentials.json', encoding="utf-8") as f:
-        credz = json.load(f)
+    credentials = read_file('tweeter-api-credentials.json')
+    keywords = read_file('person-config.json')
 
-    auth = tweepy.OAuthHandler(credz['consumer-key']['key'], credz['consumer-key']['secret'])
-    auth.set_access_token(credz['access-token']['token'], credz['access-token']['secret'])
+    auth = tweepy.OAuthHandler(credentials['consumer-key']['key'],
+                               credentials['consumer-key']['secret'])
+
+    auth.set_access_token(credentials['access-token']['token'],
+                          credentials['access-token']['secret'])
+
     api = tweepy.API(auth)
-
     tweet_stream = tweepy.Stream(auth=api.auth, listener=SatStreamListener(logger=logger))
-    tweet_stream.filter(track=['Trump'], async=False)
+    tweet_stream.filter(track=keywords['keywords'], async=False)
 
 
 pid_file = '/tmp/kyc-producer.pid'
