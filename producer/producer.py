fed1f3641fb6ff68d8fda8773b65a8570e302a51
Eddy Medvednic <17541522+medvednic@users.noreply.github.com>
2018-04-05 14:21:00 +0300

    producer - RabbitMQ is used to publish incoming tweets, some refactoring

--- producer/producer.py
+++ producer/producer.py
@@ -1,9 +1,11 @@
-import tweepy
 import logging
-import json
 
-from listener import SatStreamListener
+import pika
+import tweepy
 from daemonize import Daemonize
+
+import file_reader
+from listener import SatStreamListener
 
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.DEBUG)
@@ -14,27 +16,43 @@ logger.addHandler(fh)
 keep_fds = [fh.stream.fileno()]
 
 
-def read_file(file_name):
-    with open('config/' + file_name, encoding="utf-8") as f:
-        return json.load(f)
+def mq_chanel_connect(host, queue_name):
+    broker_connection = pika.BlockingConnection(pika.ConnectionParameters(host))
+    channel = broker_connection.channel()
+    channel.queue_declare(queue=queue_name)
+    return channel
 
 
-def main():
-    logging.info('Session start')
-    credentials = read_file('tweeter-api-credentials.json')
-    keywords = read_file('person-config.json')
-
+def twitter_api_init(credentials):
     auth = tweepy.OAuthHandler(credentials['consumer-key']['key'],
                                credentials['consumer-key']['secret'])
 
     auth.set_access_token(credentials['access-token']['token'],
                           credentials['access-token']['secret'])
 
-    api = tweepy.API(auth)
-    tweet_stream = tweepy.Stream(auth=api.auth, listener=SatStreamListener(logger=logger))
-    tweet_stream.filter(track=keywords['keywords'], async=False)
+    return tweepy.API(auth)
+
+
+def main():
+    logging.info('Session start')
+    credentials = file_reader.read_json('tweeter-api-credentials.json')
+    keywords = file_reader.read_json('person-config.json')
+
+    twitter_api = twitter_api_init(credentials)
+    mq_channel = mq_chanel_connect('localhost', 'tweets')
+
+    stream_listener = SatStreamListener(logger=logger,
+                                        mq_channel=mq_channel)
+
+    tweet_stream = tweepy.Stream(auth=twitter_api.auth,
+                                 listener=stream_listener)
+
+    tweet_stream.filter(track=keywords['keywords'],
+                        async=False)
 
 
 pid_file = '/tmp/kyc-producer.pid'
-daemon = Daemonize(app="kyc-producer", pid=pid_file, action=main(), keep_fds=keep_fds)
+daemon = Daemonize(app="kyc-producer",
+                   pid=pid_file, action=main(),
+                   keep_fds=keep_fds)
 daemon.start()
